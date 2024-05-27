35c1ec10d08d90abf40c95bf2c61120d325e0836
borgesenioc <borgesenioc@gmail.com>
2024-05-27 20:03:09 -0300

    Add listen command to PORT 4001 on server.js

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+//add the router variables
+
+//add a GET
+
+//add a POST
+
+//add a get per specific author or ID
+
+//  Make the server listen to the right port
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
