88bc9c7bc5b739997c45b057c2990b6ec6c06c58
borgesenioc <borgesenioc@gmail.com>
2024-05-28 09:40:13 -0300

    Add POST method to add a new quote to the data, but it's not working well

--- server.js
+++ server.js
@@ -6,32 +6,46 @@ const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
+// Middleware to serve static files
 app.use(express.static('public'));
 
-//add a route to GET a random quote
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Route to GET a random quote
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
-    res.send({quote: randomQuote});
+    res.send({ quote: randomQuote });
 });
 
-//add a GET for all quotes, or a single person if there is a query 
+// Route to GET all quotes, or a specific person's quotes if there is a query
 app.get('/api/quotes', (req, res, next) => {
     const personQuery = req.query.person;
-
     if (personQuery) {
         const personQuotes = quotes.filter(element => element.person === personQuery);
-        if (personQuotes.length > 0) {
-            res.send({ quotes: personQuotes}); // return a specific person's quotes
-        } else {
-            res.send({ quotes: [] }); // returns an emptyArray
-        };
+        res.send({ quotes: personQuotes });
     } else {
-        res.send({ quotes: quotes }) // returns all quotes
+        res.send({ quotes: quotes });
     }
 });
 
+// Route to POST a new quote
+app.post('/api/quotes', (req, res, next) => {
+    const newQuote = req.body.quote;
+    const newPerson = req.body.person;
 
-//  Make the server listen to the right port
+    console.log('Received new quote:', req.body);
+
+    if (newQuote && newPerson) {
+        const newObject = { quote: newQuote, person: newPerson };
+        quotes.push(newObject);
+        res.status(201).send(newObject);
+    } else {
+        res.status(400).send({ error: 'Both quote and person fields are required' });
+    }
+});
+
+// Make the server listen to the right port
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
