ad94756a09a37008476152547810bcf67ee057c4
borgesenioc <borgesenioc@gmail.com>
2024-05-28 09:02:36 -0300

    Add method to GET all quotes or from a specific author

--- server.js
+++ server.js
@@ -11,14 +11,25 @@ app.use(express.static('public'));
 //add a route to GET a random quote
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
-    res.send(randomQuote);
+    res.send({quote: randomQuote});
 });
 
-//add a GET
+//add a GET for all quotes, or a single person if there is a query 
+app.get('/api/quotes', (req, res, next) => {
+    const personQuery = req.query.person;
 
-//add a POST
+    if (personQuery) {
+        const personQuotes = quotes.filter(element => element.person === personQuery);
+        if (personQuotes.length > 0) {
+            res.send({ quotes: personQuotes}); // return a specific person's quotes
+        } else {
+            res.send({ quotes: [] }); // returns an emptyArray
+        };
+    } else {
+        res.send({ quotes: quotes }) // returns all quotes
+    }
+});
 
-//add a get per specific author or ID
 
 //  Make the server listen to the right port
 app.listen(PORT, () => {
